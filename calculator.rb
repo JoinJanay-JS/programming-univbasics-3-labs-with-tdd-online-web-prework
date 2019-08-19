da3e0abf9608621ef24c9cab75bb281d900db20b
Janay Bailey <tjanay87@gmail.com>
2019-08-19 15:23:21 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,4 @@
 # Add your variables here
 ```first_number = "Hello World"
 second_number = "2"
-expect second_number not_to equal (0)
+expect second_number nothttps://support.microsoft.com/en-us/help/4512508/windows-10-update-kb4512508_to equal (0)
